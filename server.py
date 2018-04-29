5f94f8607bbd656711d4fcf5a8675544911da4dd
Joshua Rasmussen <xlr8runner@gmail.com>
2018-04-29 15:33:02 -0700

    Simplified the server

--- server.py
+++ server.py
@@ -4,63 +4,38 @@ from flask import Flask, request, render_template
 from flask.views import View
 from leds import LEDColor, LEDFactory
 
-def opt_parse():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--num', default=10, type=int)
+LED_COUNT = 275
+DEBUG = True
+PORT = 3000
 
-    feature_parser = parser.add_mutually_exclusive_group(required=False)
-    feature_parser.add_argument('--dev', dest='debug', action='store_true')
-    feature_parser.add_argument('--prod', dest='debug', action='store_false')
-    parser.set_defaults(debug=True)
-    return parser.parse_args()
+app = Flask(__name__)
+ledstrip = LEDFactory.make(LED_COUNT)
+ledstrip.setup()
+ledstrip.show()
 
-class BaseLEDView(View):
-    def __init__(self, strip):
-        self.strip = strip
+@app.route('/color/<int:index>', methods=['POST'])
+def setPixelAPI(index):
+    color_json = request.get_json()
+    color = LEDColor(color_json['red'], color_json['green'], color_json['blue'])
+    ledstrip.setPixelColor(index, color, True)
 
-    def parse_color(self):
-        color_json = request.get_json()
-        self.color = LEDColor(color_json['red'], color_json['green'], color_json['blue'])
+    return 'OK'
 
-class SetPixelAPI(BaseLEDView):
-    methods = ['POST']
-    def dispatch_request(self, index):
-        self.parse_color()
-        self.strip.setPixelColor(index, self.color, True)
+@app.route('/color', methods=['POST'])
+def batchSetPixelAPI():
+    color_json = request.get_json()
+    color = LEDColor(color_json['red'], color_json['green'], color_json['blue'])
 
-        return 'OK'
+    for i in range(ledstrip.size()):
+        ledstrip.setPixelColor(i, color)
 
-class BatchSetPixelAPI(BaseLEDView):
-    methods = ['POST']
-    def dispatch_request(self):
-        self.parse_color()
-        for i in range(self.strip.size()):
-            self.strip.setPixelColor(i, self.color)
-        self.strip.show()
+    ledstrip.show()
 
-        return 'OK'
+    return 'OK'
 
+@app.route('/')
 def render_home():
     return render_template('index.html')
 
-class LEDServer:
-    def __init__(self, size, debug):
-        self.app = Flask(__name__)
-        self.debug = debug
-        self.ledstrip = LEDFactory.make(size)
-
-    def register_views(self):
-        self.app.add_url_rule('/', view_func=render_home)
-        self.app.add_url_rule('/color/<int:index>', view_func=SetPixelAPI.as_view('set_pixel', strip=self.ledstrip))
-        self.app.add_url_rule('/color', view_func=BatchSetPixelAPI.as_view('batch_set_pixel', strip=self.ledstrip))
-
-    def start(self):
-        self.ledstrip.setup()
-        self.ledstrip.show()
-        self.app.run(host='0.0.0.0', port=3000, debug=self.debug)
-
 if __name__ == "__main__":
-    args = opt_parse()
-    s = LEDServer(size=args.num, debug=args.debug)
-    s.register_views()
-    s.start()
+    app.run(host='0.0.0.0', port=PORT, debug=DEBUG)
